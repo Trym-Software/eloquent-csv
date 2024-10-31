2d5ffd0ece708e61a797783b826f94d66afa5aee
Luke Spencer <luke@trym.software>
2024-10-31 21:22:08 +0000

    Convert CSV reading to fgetcsv

--- src/CsvFile.php
+++ src/CsvFile.php
@@ -6,22 +6,37 @@ use Illuminate\Support\Collection;
 
 class CsvFile
 {
-    public function read(string $filename): Collection
-    {
-        $csv = file_get_contents($filename);
-        $lines = explode(PHP_EOL, $csv);
-        $header = collect(str_getcsv(array_shift($lines)));
-        $rows = collect($lines)->filter();
+    public function read(
+        string $filename,
+        string $separator = ',',
+        string $enclosure = "\"",
+        string $escape = "",
+    ): Collection{
+        $rows = collect();
+        if (($file = fopen($filename, "r")) !== false) {
+            while (($data = fgetcsv($file, null, $separator, $enclosure, $escape)) !== false) {
+                $rows[] = $data;
+            }
+            fclose($file);
+        }
 
-        return $rows->map(fn ($row) => $header->combine(str_getcsv($row)));
+        $header = $rows->shift();
+
+        return $rows->filter()->map(fn ($row) => $header->combine($row));
     }
 
-    public function write(string $filename, Collection $rows): void
-    {
+    public function write(
+        string $filename,
+        Collection $rows,
+        string $separator = ',',
+        string $enclosure = "\"",
+        string $escape = "",
+        string $eol = "\n"
+    ): void {
         $file = fopen($filename, 'w');
 
         fputcsv($file, collect($rows->first())->keys()->all());
-        $rows->each(fn ($row) => fputcsv($file, collect($row)->all()));
+        $rows->each(fn ($row) => fputcsv($file, collect($row)->all(), $separator, $enclosure, $escape, $eol));
 
         fclose($file);
     }
