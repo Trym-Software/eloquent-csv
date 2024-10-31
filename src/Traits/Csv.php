eb786572bf5b7d442d4b8ced5d17bacfb90ed3f6
Luke Spencer <luke@trym.software>
2024-10-31 20:06:30 +0000

    Added trait

---
+++ src/Traits/Csv.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace EloquentCsv\Traits;
+
+use EloquentCsv\Facades\CSV as Facade;
+use Sushi\Sushi;
+
+trait Csv
+{
+    use Sushi;
+
+    public $csvFile;
+
+    public function csvFile()
+    {
+        return $this->csvFile;
+    }
+
+    public function getRows()
+    {
+        return Facade::read($this->csvFile())->toArray() ?? [];
+    }
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope('remove-id', function (Builder $builder) {
+            $builder->setHidden('id');
+        });
+    }
+}
